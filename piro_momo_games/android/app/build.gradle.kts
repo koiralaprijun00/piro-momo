edbd772b9aeda26ca2538e305058c776d2bf4f64
Prijun Koirala <191650188+koiralaprijun00@users.noreply.github.com>
2025-11-13 04:50:15 +0200

    Update versionCode and versionName in build config
    Set versionCode to 2 and versionName to 1.0.1 in android/app/build.gradle.kts for release versioning. Removed references to Flutter variables for these fields.

--- piro_momo_games/android/app/build.gradle.kts
+++ piro_momo_games/android/app/build.gradle.kts
@@ -30,14 +30,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.piro_momo_games"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
