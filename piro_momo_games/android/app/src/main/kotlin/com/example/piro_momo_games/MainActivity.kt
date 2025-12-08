9e5634ce09974736435d513b13654099be8d4d71
Prijun Koirala <191650188+koiralaprijun00@users.noreply.github.com>
2025-12-08 22:53:39 +0200

    Reorganize: separate web and mobile apps into folders

--- piro_momo_games/android/app/src/main/kotlin/com/example/piro_momo_games/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.piro_momo_games
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- piro_momo_games/lib/features/games/logo_quiz/application/logo_quiz_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'logo_quiz_controller.dart';
-import 'logo_quiz_state.dart';
-
-final logoQuizControllerProvider =
-    StateNotifierProvider.autoDispose<LogoQuizController, LogoQuizState>((ref) {
-  return LogoQuizController();
-});
